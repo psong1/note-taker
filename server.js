71e8ea262ad1559cc22893ef719d78abdbdd88b5
psong1 <paulsong29@gmail.com>
2023-03-14 23:27:50 -0400

    added apiroute

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
