5cec780972fb7be380b9c35fcde6f600297f8fe0
psong1 <paulsong29@gmail.com>
2023-03-16 23:13:35 -0400

    fixed api routes path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/APIRoutes');
 const htmlRoutes = require('./routes/HTMLRoutes');
 
 app.use(express.urlencoded({ extended: true }));
